c2c9e87a3f172c518ff50d9fc26ff61d6c14207f
Vladislav Ilin <vladislav.ilin@hiq.ru>
2019-04-18 09:40:34 +0300

    display items in the template

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ItemService } from "src/app/service/item.service";
+import { Item } from "src/app/models/item";
 
 @Component({
   selector: "app-items",
@@ -7,11 +8,13 @@ import { ItemService } from "src/app/service/item.service";
   styleUrls: ["./items.component.scss"]
 })
 export class ItemsComponent implements OnInit {
+  items: Item[];
   constructor(private itemService: ItemService) {}
 
   ngOnInit() {
     this.itemService.getItems().subscribe(items => {
-      console.log(items);
+      // console.log(items);
+      this.items = items;
     });
   }
 }
