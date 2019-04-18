1d90dcea1517a1b42ca2a1f010000b41b8110cb8
Vladislav Ilin <vladislav.ilin@hiq.ru>
2019-04-18 09:36:57 +0300

    fetching items from firestore

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ItemService } from "src/app/service/item.service";
 
 @Component({
-  selector: 'app-items',
-  templateUrl: './items.component.html',
-  styleUrls: ['./items.component.scss']
+  selector: "app-items",
+  templateUrl: "./items.component.html",
+  styleUrls: ["./items.component.scss"]
 })
 export class ItemsComponent implements OnInit {
-
-  constructor() { }
+  constructor(private itemService: ItemService) {}
 
   ngOnInit() {
+    this.itemService.getItems().subscribe(items => {
+      console.log(items);
+    });
   }
-
 }
--- src/app/service/item.service.ts
+++ src/app/service/item.service.ts
@@ -1,11 +1,24 @@
-import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
-
+import { Injectable } from "@angular/core";
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument
+} from "@angular/fire/firestore";
+import { Item } from "../models/item";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ItemService {
+  itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
 
-  constructor() { }
+  constructor(public afs: AngularFirestore) {
+    this.items = this.afs.collection("items").valueChanges();
+  }
+
+  getItems() {
+    return this.items;
+  }
 }
